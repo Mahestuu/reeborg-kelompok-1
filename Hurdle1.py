af2b76fd946d65eb73d337e2cac0ac1f7fcd64cc
Mahestuu <149101247+Mahestuu@users.noreply.github.com>
2023-11-16 23:28:56 +0700

    reeborg hurdle 1

---
+++ Hurdle1.py
@@ -0,0 +1,20 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def step():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+step()
+step()
+step()
+step()
+step()
+step()
