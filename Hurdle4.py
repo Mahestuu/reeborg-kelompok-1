7ce3c45e669ed6084ffd365ae69fcb6e9764753f
Salzabila <149697500+Salzabila@users.noreply.github.com>
2023-11-17 00:02:46 +0700

    reeborg hurdle 4

---
+++ Hurdle4.py
@@ -0,0 +1,11 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+        turn_right()
+    else:
+        turn_left()
